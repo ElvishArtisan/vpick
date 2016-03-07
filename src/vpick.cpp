94ef8c07b03d7c40fadd1d555662a367646e3150
Fred Gleason <fredg@paravelsystems.com>
2016-03-07 12:59:03 -0500

    Initial import

---
+++ src/vpick.cpp
@@ -0,0 +1,54 @@
+// vpick.cpp
+//
+// vpick(1) Host Chooser
+//
+//   (C) Copyright 2016 Fred Gleason <fredg@paravelsystems.com>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public
+//   License along with this program; if not, write to the Free Software
+//   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#include <QApplication>
+
+#include "vpick.h"
+
+MainWidget::MainWidget(QWidget *parent)
+  : QMainWindow(parent)
+{
+}
+
+
+QSize MainWidget::sizeHint() const
+{
+  return QSize(400,300);
+}
+
+
+void MainWidget::closeEvent(QCloseEvent *e)
+{
+  qApp->quit();
+}
+
+
+void MainWidget::resizeEvent(QResizeEvent *e)
+{
+}
+
+
+int main(int argc,char *argv[])
+{
+  QApplication a(argc,argv);
+  MainWidget *w=new MainWidget();
+  w->show();
+  return a.exec();
+}
